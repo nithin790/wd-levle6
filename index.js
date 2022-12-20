675474099fb6ff250d22fea48e5ae58960427ce7
RANAMALLA NITHIN REDDY <113250052+nithin790@users.noreply.github.com>
2022-12-20 07:02:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 
-app.listen(3000, () => {
+// eslint-disable-next-line no-undef
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log("Started express server at port 3000");
-});
+});v
