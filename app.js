0f1140a2b5b19950c7bbc5d82e3427e5be2e758c
RANAMALLA NITHIN REDDY <113250052+nithin790@users.noreply.github.com>
2022-12-22 08:35:45 -0600

    Initial commit

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
+/* eslint-disable no-var */
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
+/* eslint-disable quotes */
 const express = require("express");
+var csrf = require("tiny-csrf");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 const path = require("path");
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", async (request, response) => {
+  const overdue = await Todo.overdue();
+  const dueToday = await Todo.dueToday();
+  const dueLater = await Todo.dueLater();
+  if (request.accepts("html")) {
+    response.render("index", {
+      title: "Todo application",
+      overdue,
+      dueToday,
+      dueLater,
+      csrfToken: request.csrfToken(),
+    });
+  } else {
+    response.json({
+      overdue,
+      dueToday,
+      dueLater,
+    });
+  }
+});
+
+app.get("/todos", async function (request, response) {
+  console.log("Processing list of all Todos ...");
   try {
-    const overduetodos = await Todo.overdue();
-    const duetodaytodos = await Todo.dueToday();
-    const duelatertodos = await Todo.dueLater();
-    if (request.accepts("html")) {
-      response.render("index", {
-        title: "To-Do Manager",
-        overduetodos,
-        duetodaytodos,
-        duelatertodos,
-      });
-    } else {
-      response.json({
-        overduetodos,
-        duetodaytodos,
-        duelatertodos,
-      });
-    }
+    const todos = await Todo.findAll();
+    return response.send(todos);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
-});
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/todos", async function (_request, response) {
-  try {
-    const todos = await Todo.findAll({
-      order: [["id", "ASC"]],
-    });
-    return response.json(todos);
-  } catch (error) {
-    console.log(error);
-    return response.status(500).send(error);
-  }
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // Find all users
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -54,8 +64,12 @@ app.get("/todos/:id", async function (request, response) {
 });
 
 app.post("/todos", async function (request, response) {
+  console.log("Creating a todo", request.body);
   try {
-    const todo = await Todo.addTodo({title: request.body.title, dueDate: request.body.dueDate});
+    await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
     return response.redirect("/");
   } catch (error) {
     console.log(error);
@@ -74,25 +88,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
   }
 });
 
-app.delete("/todos/:id", async function (request, response) {
-  console.log("We have to delete a Todo with ID: ", request.params.id);
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our database to delete a Todo by ID.
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
+app.delete("/todos/:id", async (request, response) => {
+  console.log("delete a Todo with ID: ", request.params.id);
   try {
-    const deltodo = await Todo.destroy({
-      where: {
-        id: request.params.id,
-      },
-    });
-    console.log(deltodo);
-    return response.send(deltodo == true);
+    await Todo.remove(request.params.id);
+    return response.json({ success: true });
   } catch (error) {
-    console.log(error);
     return response.status(422).json(error);
   }
 });
-
 module.exports = app;
